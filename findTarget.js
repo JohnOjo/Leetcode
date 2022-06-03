43d76a3ea0b4a4024eb019ed0d00003b63664f76
johnojo97 <jojo@retrorabbit.co.za>
2022-06-03 22:22:20 +0200

    Did some binary search trees

--- findTarget.js
+++ findTarget.js
@@ -1,31 +1,35 @@
 var findTarget = function(root, k) {
-    const values = []
+  const values = []
   
-    if (root) {
-        recursiveFindTarget(root, values)
+  if (root) {
+      recursiveFindTarget(root, values)
+  }
+  
+  let i = 0
+  for (const value of values) {
+    if(values.find((v, index) => v + value === k && index !== i)){
+      return true
     }
-  
-    for (const value of values) {
-      if(values.filter((v) => v + value === k).length > 0){
-        return true
-      }
-    }
-  
-    return false
-  };
-  
-  var recursiveFindTarget = function(node, values) {
-    node.val != null && values.push(node.val)
-  
-    if(node.left) {
-      findTarget(node.left, values)
-    }
-  
-    if(node.right) {
-      findTarget(node.right, values)
-    }
-  
-    if(!node.left && !node.right) {
-      return
-    }
-  };
+    i++
+  }
+
+  return false
+};
+
+var recursiveFindTarget = function(node, values) {
+  values.push(node.val)
+
+  if(node.left) {
+    recursiveFindTarget(node.left, values)
+  }
+
+  if(node.right) {
+    recursiveFindTarget(node.right, values)
+  }
+
+  if(!node.left && !node.right) {
+    return
+  }
+};
+
+console.log(findTarget(null, 1))
